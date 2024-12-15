07a9dfb908470c61504f15fcbf068bde3f446cef
Mauricio Molina <tica.molina57@gmail.com>
2024-12-14 21:45:07 -0300

    Resuelvo el ejercicio A utilizando la estructura For y dentro del mismo un if en donde tomo como referencia el resto o modulo de cada i para representar un mensaje en consola con un gato distinto, en el mensaje en consola uso concatenacion con +

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 import './style.css'
+
+
+
+console.warn('Ejercicio A')
+
+let cantidadDeGatos = 10
+
+for (let i = 1; i <= cantidadDeGatos; i++) {
+  if (i % 3 === 0) {
+   console.log("Gato #" + i + ": 😹");
+} else if (i % 3 === 1) {
+   console.log("Gato #" + i + ": 😺");
+} else {
+   console.log("Gato #" + i + ": 😸");
+}}
+
+//---------------------------------------------------------
+
+
+
